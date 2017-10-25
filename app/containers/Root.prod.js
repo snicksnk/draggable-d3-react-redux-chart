3cd4c54035a1b20588ae33eff0e0c9d69fd2d15a
Yuri <snicksnk@MacBook-Pro-Yuri.local>
2017-10-25 04:01:23 +0400

    gh-pages build

--- app/containers/Root.prod.js
+++ app/containers/Root.prod.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import routes from '../routes';
 import { Router } from 'react-router';
+import routes from '../routes';
 
 export default class Root extends Component {
   render() {
